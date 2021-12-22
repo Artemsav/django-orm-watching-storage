126cbd2988fa22663d4919a2344011261a902097
artemsav <studentbedn@gmail.com>
2021-12-22 16:29:01 +0300

    fix manage.py, delete unnessary cod

--- manage.py
+++ manage.py
@@ -1,15 +1,7 @@
 #!/usr/bin/venv python
 import os
 import sys
-from dotenv import load_dotenv
-from pathlib import Path
-from environs import Env
 
-env_path = Path('/home/hisp/Projects/django-orm-watching-storage/project', '.env')
-env = Env()
-env.read_env()
-load_dotenv(dotenv_path=env_path,override=True)
-DEBUG = env.bool("DEBUG", default=False)
 
 if __name__ == "__main__":
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "project.settings")
